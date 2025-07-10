d8aea6f0b5164b03c04386ceac939b3e903fa750
Matin Ghanbari <85116522+MatinGhanbari@users.noreply.github.com>
2025-07-10 19:00:17 +0330

    Update update_contributors.py

--- src/update_contributors.py
+++ src/update_contributors.py
@@ -7,6 +7,8 @@ def update_contributors():
     response = requests.get(url)
     if response.status_code == 200:
         contributors = response.json()
+        owner = next((item for item in data if item.get("id") == 2), None)
+        contributors.remove(owner)
         output_file_path = r"public/assets/data/contributors.json"
         with open(output_file_path, 'w+', encoding="utf-8") as json_file:
             json.dump(contributors, json_file, indent=4)
